20397b10b8e0a6dd1dd11563882fdb08e941846f
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-08-15 00:40:47 +0300

    task1, task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryList: document.querySelector('.gallery'),
+};
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+refs.galleryList.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const throttle = require('lodash.throttle');
 
+const STORAGE_KEY_MSG = 'feedback-message';
+
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+
+getTextArea();
+
+function onFeedbackFormSubmit(evt) {
+  evt.preventDefaul();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY_MSG);
+}
+
+function onTextAreaInput(evt) {
+  const message = evt.target.value;
+  localStorage.setItem(STORAGE_KEY_MSG, message);
+}
+
+function getTextArea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY_MSG);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
